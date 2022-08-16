87325e0f43d0ba790630c47d6c141856f974950f
Joana Neuenschwander <jo.neush@gmail.com>
2022-08-16 14:59:56 +0100

    Create the index, show, new and create methods on the TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,29 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+# Quando uso o form_for, ele já vem com o authenticity token. Então preciso só fazer:
+# params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
