7dbd3978043708dcd5ba613de8332ab48e8e2c19
Joana Neuenschwander <jo.neush@gmail.com>
2022-08-16 15:47:21 +0100

    Create the edit, update and destroy method and the respective views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,12 +18,29 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params) # params[:task] would raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+    # status: :see_other responds with a 303 HTTP status code
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
-# Quando uso o form_for, ele já vem com o authenticity token. Então preciso só fazer:
-# params.require(:task).permit(:title, :details, :completed)
+    # Quando uso o form_for, ele já vem com o authenticity token. Então preciso só fazer:
+    # params.require(:task).permit(:title, :details, :completed)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
   get "tasks/new", to: "tasks#new"
+  # Tenho que colocar o tasks/new antes de tasks/:id porque senão vai achar que new é
+  # é um parâmetro, pois começam tem o mesmo VERB e ambos começam com tasks.
   post "tasks", to: "tasks#create"
   get 'tasks/:id', to: 'tasks#show', as: :task
-
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task # para ler o form
+  patch "tasks/:id", to: "tasks#update" # enviar as respostas com o submit
+  delete "tasks/:id", to: "tasks#destroy"
 end
